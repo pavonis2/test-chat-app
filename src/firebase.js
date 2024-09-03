90bc4616199c811b70db9256406b2ef353f7ee47
Rohan <rohanmv22@gmail.com>
2024-09-03 16:34:54 +0530

    added env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,14 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 import {getStorage} from "firebase/storage"
-import firebaseCredentials from "./firebasecredentials";
-
 
 const firebaseConfig = {
-  apiKey: firebaseCredentials.api_Key,
-  authDomain: firebaseCredentials.auth_Domain,
-  projectId: firebaseCredentials.project_Id,
-  storageBucket: firebaseCredentials.storage_Bucket,
-  messagingSenderId: firebaseCredentials.messaging_SenderId,
-  appId: firebaseCredentials.app_Id
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
