f009539952500533f0f3bb8c0883de0770df6fc6
Rohan <rohanmv22@gmail.com>
2024-09-04 23:29:16 +0530

    Removed unnecessary comments and fixed the styling

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,55 +1,6 @@
-/* import { onAuthStateChanged } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase";
-
-export const AuthContext = createContext();
-
-export const AuthContextProvider = ({children}) => {
-  const [currentUser, setCurrentUser] = useState({})
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-      console.log(user);
-    })
-  }, []) 
-}
-
-<AuthContext.Provider value={{currentUser}}>
-  {children}
-</AuthContext.Provider> */
-/* import { createContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
-
-export const AuthContext = createContext();
-
-export const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState({});
-
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-      // console.log(user);
-    });
-
-    return () => {
-      unsub();
-    };
-  }, []);
-
-  return (
-    <AuthContext.Provider value={{ currentUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
-}; */
-
-
-import { createContext, useEffect, useState } from "react";
-import { auth, /* db */ } from "../firebase";
-import { onAuthStateChanged } from "firebase/auth";
-/* import { doc, getDoc, updateDoc } from "firebase/firestore";*/
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
