87021c29200197979592df62b0ffe1ddbf90851b
Rohan <rohanmv22@gmail.com>
2024-10-13 10:14:02 +0530

    Lazy loading for the register, login and home pages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import { useContext } from "react";
-import Home from "./pages/Home";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Register";
 import './style.scss'
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
-import { AuthContext } from "./context/AuthContext";
+import React, { Suspense, lazy, useContext } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
+import { ClipLoader } from 'react-spinners';
+
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/Login').then(module => ({default: module.Login})));
+const Register = lazy(() => import('./pages/Register').then(module => ({default: module.Register})));
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
 
-  const {currentUser} = useContext(AuthContext)
-
-  const ProtectedRoute = ({children}) => {
-    if(!currentUser) {
-      return <Navigate to="/login"/>
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
     }
-    return children
-  }
+    return children;
+  };
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<ProtectedRoute> 
-            <Home />
-          </ProtectedRoute>
-          }/>
-          <Route path="login" element={<Login />}/>
-          <Route path="register" element={<Register />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>  
+      <Suspense fallback={<div className="loading"><ClipLoader /></div>}>
+        <Routes>
+          <Route path="/">
+            <Route index element={<ProtectedRoute> 
+              <Home />
+            </ProtectedRoute>
+            }/>
+            <Route path="login" element={<Login />}/>
+            <Route path="register" element={<Register />}/>
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
